Banogrono/FiLink

commit 646f17a4ab39c735b5c1c4ae1c1a09ff296078dc

Fix bug allowing to manually add IP address that already exists. (issue #3)

FiLink/ViewModels/SettingsWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Xml.Serialization;
 using FiLink.Models;
@@ -137,32 +138,46 @@ namespace FiLink.ViewModels
         /// </summary>
         public void ApplySettings()
         {
-            if (!CheckFileFolder(FileFolder))
+            try
             {
-                StatusLabel = "Entered file directory path is incorrect.";
-                return;
-            }
+                if (!CheckFileFolder(FileFolder))
+                {
+                    StatusLabel = "Entered file directory path is incorrect.";
+                    return;
+                }
             
-            if (!CheckNewHostIp(HostIp))
+                if (!CheckNewHostIp(HostIp))
+                {
+                    StatusLabel = "Entered IP address is invalid.";
+                    return;
+                }
+
+                if (CheckIfIpAddressExists(HostIp))
+                {
+                    StatusLabel = "That host address already exists.";
+                    return;
+                }
+                ParentViewModel.HostCollection?.Add(HostIp);
+            
+                if (!CheckIpRange(IpRange))
+                {
+                    StatusLabel = "Entered IP Range is invalid";
+                    return;
+                }
+            
+                if (!CheckEncryptionKey(EncryptionKey))
+                {
+                    StatusLabel = "Entered encryption key is invalid";
+                    return;
+                }
+            
+                StatusLabel = "All settings applied";
+            }
+            catch (Exception e)
             {
-                StatusLabel = "Entered IP address is invalid.";
-                return;
+                UtilityMethods.LogToFile(e.ToString());
+                StatusLabel = "Something gone wrong...";
             }
-            ParentViewModel.HostCollection?.Add(HostIp);
-            
-            if (!CheckIpRange(IpRange))
-            {
-                StatusLabel = "Entered IP Range is invalid";
-                return;
-            }
-            
-            if (!CheckEncryptionKey(EncryptionKey))
-            {
-                StatusLabel = "Entered encryption key is invalid";
-                return;
-            }
-            
-            StatusLabel = "All settings applied";
         }
         
         /// <summary>
@@ -188,6 +203,21 @@ namespace FiLink.ViewModels
         // ================================================================================
         // Private Methods
         // ================================================================================
+        
+        /// <summary>
+        /// Checks if given IP address already exists in IP collection.
+        /// </summary>
+        /// <param name="ipAddress"> New IP address</param>
+        /// <returns>True if address does exist, and false if it does not exist.</returns>
+        /// <exception cref="Exception">Throws exception when Host IP collection (ParentViewModel.HostCollection) is a null.</exception>
+        private bool CheckIfIpAddressExists(string ipAddress)
+        {
+            if (ParentViewModel.HostCollection == null)
+            {
+                throw new Exception("Main Host collection is null");
+            }
+            return ParentViewModel.HostCollection.Any(address => ipAddress.Contains(address));
+        }
         
         /// <summary>
         /// Checks the correctness of file folder path.

commit 9ee1393bd83eb16ea239d6d20e48dca9fc5dd93a

* Fix constructor of settings window - replace static data with link to class containing saved settings. * Fix duplicate IP addresses when saving settings (issue #2 - partially fixed by commit 646f17a). * Fix bug in method introduced by commit 646f17a.

FiLink/Models/SerializableSettings.cs
@@ -36,5 +36,15 @@
             FileDirectory = fileDirectory;
             PingTimeout = pingTimeout;
         }
+
+        public SerializableSettings()
+        {
+            LogFileName = SettingsAndConstants.LogFileName;
+            SavedHostsFileName = SettingsAndConstants.SavedHostsFileName;
+            LowerIpAddress = SettingsAndConstants.LowerIpAddress;
+            UpperIpAddress = SettingsAndConstants.UpperIpAddress;
+            FileDirectory = SettingsAndConstants.FileDirectory;
+            PingTimeout = SettingsAndConstants.PingTimeout;
+        }
     }
 }

FiLink/ViewModels/SettingsWindowViewModel.cs
@@ -123,9 +123,9 @@ namespace FiLink.ViewModels
             IpRange = SettingsAndConstants.LowerIpAddress + "-" + SettingsAndConstants.UpperIpAddress;
             HostIp = "";
             StatusLabel = "";
-            EncryptionKey = "156156";
+            EncryptionKey = SettingsAndConstants.EncryptionKey.ToString();
             Encryption = false;
-            FileFolder = "Received_Files";
+            FileFolder = SettingsAndConstants.FileDirectory;
             PingTimeout = SettingsAndConstants.PingTimeout.ToString();
         }
         
@@ -185,9 +185,9 @@ namespace FiLink.ViewModels
         /// </summary>
         public void SaveSettings()
         {
-            ApplySettings();
             try
             {
+                ApplySettings();
                 XmlSerializer xmlSerializer = new(typeof(SerializableSettings));
                 TextWriter writer = new StreamWriter("settings.xml");
                 xmlSerializer.Serialize(writer, SettingsAndConstants.GetSerializableSettings());
@@ -212,11 +212,12 @@ namespace FiLink.ViewModels
         /// <exception cref="Exception">Throws exception when Host IP collection (ParentViewModel.HostCollection) is a null.</exception>
         private bool CheckIfIpAddressExists(string ipAddress)
         {
+            if (ipAddress == "") return false;
             if (ParentViewModel.HostCollection == null)
             {
                 throw new Exception("Main Host collection is null");
             }
-            return ParentViewModel.HostCollection.Any(address => ipAddress.Contains(address));
+            return ParentViewModel.HostCollection.Any(address => ipAddress.Equals(address));
         }
         
         /// <summary>